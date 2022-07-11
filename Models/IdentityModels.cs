e806ba40fc3e41b9eb36ec19d40fc8a6f0388065
TemiAfikode <babalolaife@yahoo.com>
2022-07-11 18:53:52 -0400

    Added view models and dbset for database models

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -27,6 +27,8 @@ namespace HTTP_5212_RNA_Group4_HospitalProject.Models
 
         public DbSet<Pharmacy> Pharmacies { get; set; }
         public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
 
         public static ApplicationDbContext Create()
         {
