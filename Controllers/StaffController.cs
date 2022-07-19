867c516467fda56dbe4a1027cd9ec7222beb94a7
TemiAfikode <babalolaife@yahoo.com>
2022-07-19 19:28:26 -0400

    Added StaffController and StaffDataController

---
+++ Controllers/StaffController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HTTP_5212_RNA_Group4_HospitalProject.Models;
+using HTTP_5212_RNA_Group4_HospitalProject.Models.ViewModels;
+using System.Web.Script.Serialization;
+using System.Net.Http;
+
+namespace HTTP_5212_RNA_Group4_HospitalProject.Controllers
+{
+    public class StaffController : Controller
+    {
+        private static readonly HttpClient client;
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+
+        static StaffController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44353/api/");
+        }
+
+        // GET: Staff
+        public ActionResult List()
+        {
+            string url = "staffdata/liststaffs";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            IEnumerable<StaffDto> Staffs = response.Content.ReadAsAsync<IEnumerable<StaffDto>>().Result;
+
+            return View(Staffs);
+        }
+
+        // GET: Staff/Details/5
+        public ActionResult Details(int id)
+        {
+            DetailsStaff ViewModel = new DetailsStaff();
+
+            string url = "staffdata/findstaff/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            StaffDto SelectedStaff = response.Content.ReadAsAsync<StaffDto>().Result;
+
+            ViewModel.SelectedStaff = SelectedStaff;
+
+            //url = "prescriptiondata/listprescriptionsforpharmacy" + id;
+            //response = client.GetAsync(url).Result;
+            //IEnumerable<PrescriptionDto> RelatedPrescriptions = response.Content.ReadAsAsync<IEnumerable<PrescriptionDto>>().Result;
+
+            //ViewModel.RelatedPrescriptions = RelatedPrescriptions;
+
+            return View(ViewModel);
+        }
+
+        //GET: Staff/New
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        // POST: Staff/Create
+        [HttpPost]
+        public ActionResult Create(Staff Staff)
+        {
+            string url = "staffdata/addstaff";
+
+            string jsonpayload = jss.Serialize(Staff);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            return RedirectToAction("List");
+        }
+
+        // GET: Staff/Edit/5
+        public ActionResult Edit(int id)
+        {
+            string url = "staffdata/findstaff/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            StaffDto selectedstaff = response.Content.ReadAsAsync<StaffDto>().Result;
+
+            return View(selectedstaff);
+        }
+
+        // POST: Staff/Update/5
+        [HttpPost]
+        public ActionResult Edit(int id, Staff Staff)
+        {
+            string url = "staffdata/updatestaff/" + id;
+            string jsonpayload = jss.Serialize(Staff);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("/Details/" + Staff.StaffId);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        public ActionResult DeleteConfirm(int id)
+        {
+            string url = "staffdata/findstaff/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            StaffDto selectedstaff = response.Content.ReadAsAsync<StaffDto>().Result;
+
+            return View(selectedstaff);
+        }
+
+        // POST: Staff/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            string url = "staffdata/deletestaff/" + id;
+            HttpContent content = new StringContent("");
+
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsJsonAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+    }
+}
---
+++ Controllers/StaffDataController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HTTP_5212_RNA_Group4_HospitalProject.Models;
+
+namespace HTTP_5212_RNA_Group4_HospitalProject.Controllers
+{
+    public class StaffDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/StaffData
+        [HttpGet]
+        [Route("api/StaffData/ListStaffs")]
+        [ResponseType(typeof(StaffDto))]
+        public IHttpActionResult GetStaffs()
+        {
+            List<Staff> Staffs = db.Staffs.ToList();
+            List<StaffDto> StaffsDtos = new List<StaffDto>();
+
+            Staffs.ForEach(st => StaffsDtos.Add(new StaffDto()
+            {
+                StaffId = st.StaffId,
+                FirstName = st.FirstName,
+                LastName = st.LastName,
+                Title = st.Title,
+                Image = st.Image,
+                Department = st.Department.DepartmentName,
+            }));
+
+            return Ok(StaffsDtos);
+        }
+
+        // GET: api/StaffData/5
+        [HttpGet]
+        [ResponseType(typeof(StaffDto))]
+        [Route("api/staffdata/findstaff/{id}")]
+        public IHttpActionResult FindStaff(int id)
+        {
+            Staff Staff = db.Staffs.Find(id);
+            StaffDto StaffDto = new StaffDto()
+            {
+                StaffId = Staff.StaffId,
+                FirstName = Staff.FirstName,
+                LastName = Staff.LastName,
+                Title = Staff.Title,
+                Image = Staff.Image,
+                Department = Staff.Department.DepartmentName,
+            };
+
+            if (Staff == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(StaffDto);
+        }
+
+        // PUT: api/StaffData/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        [Route("api/staffdata/updatestaff/{id}")]
+        public IHttpActionResult PutStaff(int id, Staff staff)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != staff.StaffId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(staff).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StaffExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/StaffData
+        [ResponseType(typeof(Staff))]
+        [Route("api/stafftdata/addstaff")]
+        public IHttpActionResult PostStaff(Staff staff)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Staffs.Add(staff);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = staff.StaffId }, staff);
+        }
+
+        // DELETE: api/StaffData/5
+        [ResponseType(typeof(Staff))]
+        public IHttpActionResult DeleteStaff(int id)
+        {
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            db.Staffs.Remove(staff);
+            db.SaveChanges();
+
+            return Ok(staff);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StaffExists(int id)
+        {
+            return db.Staffs.Count(e => e.StaffId == id) > 0;
+        }
+    }
+}
