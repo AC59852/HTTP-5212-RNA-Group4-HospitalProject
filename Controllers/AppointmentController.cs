3a02a4c1e45b760638bb76eb3f85188244d672dc
TemiAfikode <babalolaife@yahoo.com>
2022-07-19 19:28:55 -0400

    Added AppointmentController and AppointmentDataController

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HTTP_5212_RNA_Group4_HospitalProject.Models;
+using HTTP_5212_RNA_Group4_HospitalProject.Models.ViewModels;
+using System.Web.Script.Serialization;
+using System.Net.Http;
+
+namespace HTTP_5212_RNA_Group4_HospitalProject.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private static readonly HttpClient client;
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+
+        static AppointmentController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44353/api/");
+        }
+
+        // GET: Appointment
+        public ActionResult List()
+        {
+            string url = "appointmentdata/listappointments";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            IEnumerable<AppointmentDto> Appointments = response.Content.ReadAsAsync<IEnumerable<AppointmentDto>>().Result;
+
+            return View(Appointments);
+        }
+
+        // GET: Appointment/Details/5
+        public ActionResult Details(int id)
+        {
+            DetailsAppointment ViewModel = new DetailsAppointment();
+
+            string url = "appointmentdata/findappointment/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            AppointmentDto SelectedAppointment = response.Content.ReadAsAsync<AppointmentDto>().Result;
+
+            ViewModel.SelectedAppointment = SelectedAppointment;
+
+            //url = "prescriptiondata/listprescriptionsforpharmacy" + id;
+            //response = client.GetAsync(url).Result;
+            //IEnumerable<PrescriptionDto> RelatedPrescriptions = response.Content.ReadAsAsync<IEnumerable<PrescriptionDto>>().Result;
+
+            //ViewModel.RelatedPrescriptions = RelatedPrescriptions;
+
+            return View(ViewModel);
+        }
+
+        //GET: Appointment/New
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        // POST: Appointment/Create
+        [HttpPost]
+        public ActionResult Create(Appointment Appointment)
+        {
+            string url = "appointmentdata/addappointment";
+
+            string jsonpayload = jss.Serialize(Appointment);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            return RedirectToAction("List");
+        }
+
+        // GET: Appointment/Edit/5
+        public ActionResult Edit(int id)
+        {
+            string url = "appointmentdata/findappointment/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            AppointmentDto selectedappointment = response.Content.ReadAsAsync<AppointmentDto>().Result;
+
+            return View(selectedappointment);
+        }
+
+        // POST: Appointment/Update/5
+        [HttpPost]
+        public ActionResult Edit(int id, Appointment Appointment)
+        {
+            string url = "appointmentdata/updateappointment/" + id;
+            string jsonpayload = jss.Serialize(Appointment);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("/Details/" + Appointment.AppointmentId);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        public ActionResult DeleteConfirm(int id)
+        {
+            string url = "appointmentdata/findappointment/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            AppointmentDto selectedappointment = response.Content.ReadAsAsync<AppointmentDto>().Result;
+
+            return View(selectedappointment);
+        }
+
+        // POST: Appointment/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            string url = "appointmentdata/deleteappointment/" + id;
+            HttpContent content = new StringContent("");
+
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsJsonAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+    }
+}
---
+++ Controllers/AppointmentDataController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HTTP_5212_RNA_Group4_HospitalProject.Models;
+
+namespace HTTP_5212_RNA_Group4_HospitalProject.Controllers
+{
+    public class AppointmentDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/AppointmentData
+        [HttpGet]
+        [Route("api/AppointmentData/ListAppointments")]
+        [ResponseType(typeof(AppointmentDto))]
+        public IHttpActionResult GetAppointments()
+        {
+            List<Appointment> Appointments = db.Appointments.ToList();
+            List<AppointmentDto> AppointmentsDtos = new List<AppointmentDto>();
+
+            Appointments.ForEach(ap => AppointmentsDtos.Add(new AppointmentDto()
+            {
+                AppointmentId = ap.AppointmentId,
+                PatientName = ap.PatientName,
+                StaffName = ap.Staff.FirstName + " " + ap.Staff.LastName,
+                PatientNotes = ap.PatientNotes,
+                AppointmentDate = ap.AppointmentDate,
+            }));
+
+            return Ok(AppointmentsDtos);
+        }
+
+        // GET: api/AppointmentData/5
+        [HttpGet]
+        [ResponseType(typeof(AppointmentDto))]
+        [Route("api/appointmentdata/findappointment/{id}")]
+        public IHttpActionResult FindAppointment(int id)
+        {
+            Appointment Appointment = db.Appointments.Find(id);
+            AppointmentDto AppointmentDto = new AppointmentDto()
+            {
+                AppointmentId = Appointment.AppointmentId,
+                PatientName = Appointment.PatientName,
+                StaffName = Appointment.Staff.FirstName + " " + Appointment.Staff.LastName,
+                PatientNotes = Appointment.PatientNotes,
+                AppointmentDate = Appointment.AppointmentDate,
+            };
+
+            if (Appointment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(AppointmentDto);
+        }
+
+        // PUT: api/AppointmentData/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        [Route("api/appointmentdata/updateappointment/{id}")]
+        public IHttpActionResult PutAppointment(int id, Appointment appointment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != appointment.AppointmentId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(appointment).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppointmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/AppointmentData
+        [ResponseType(typeof(Appointment))]
+        [Route("api/appointmentdata/addappointment")]
+        public IHttpActionResult PostAppointment(Appointment appointment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Appointments.Add(appointment);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = appointment.AppointmentId }, appointment);
+        }
+
+        // DELETE: api/AppointmentData/5
+        [ResponseType(typeof(Appointment))]
+        public IHttpActionResult DeleteAppointment(int id)
+        {
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            db.Appointments.Remove(appointment);
+            db.SaveChanges();
+
+            return Ok(appointment);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AppointmentExists(int id)
+        {
+            return db.Appointments.Count(e => e.AppointmentId == id) > 0;
+        }
+    }
+}
